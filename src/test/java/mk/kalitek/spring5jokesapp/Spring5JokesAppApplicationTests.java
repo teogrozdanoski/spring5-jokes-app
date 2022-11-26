9a76505b22af1c0b62b1181015d8b42151f421b8
Teo Grozdanoski <teo.grozdanoski@gmail.com>
2022-11-26 14:44:04 +0100

    initial commit

---
+++ src/test/java/mk/kalitek/spring5jokesapp/Spring5JokesAppApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.kalitek.spring5jokesapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5JokesAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
